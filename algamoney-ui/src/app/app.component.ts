5833e2898f8090fc5c3d8089468ace4d81dcce56
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2025-03-07 10:32:40 -0300

    feat(core): implementar front end do algamoneu
    Implementa o sistema base de gestão financeira Algamoney com os seguintes recursos:
    
    feat(pessoas):
    - Implementa listagem paginada de pessoas
    - Desenvolve formulário de cadastro com validação
    
    feat(lancamentos):
    - Implementar gestão de receitas e despesas
    - Adicionar formulário de cadastro com seleção de tipo
    - Desenvolver listagem com status e valores
    - Criar filtros de pesquisa por data e descrição
    
    feat(ui):
    - Desenvolver interface responsiva com Tailwind
    - Integrar ícones FontAwesome
    - Implementar componentes reutilizáveis
    - Adicionar animações de transição
    
    feat(arch):
    - Configurar estrutura modular do projeto
    - Implementar roteamento entre componentes
    - Desenvolver template principal responsivo
    - Criar formulários reativos com validação

--- algamoney-ui/src/app/app.component.ts
+++ algamoney-ui/src/app/app.component.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import {FormsModule} from "@angular/forms";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FloatLabelModule, FormsModule],
-
+  imports: [ RouterOutlet,RouterLinkActive,RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'algamoney-ui';
+
+  isMobileMenuOpen = false;
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
--- algamoney-ui/src/app/app.routes.ts
+++ algamoney-ui/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos/lancamentos-cadastro/lancamentos-cadastro.component';
+import { PessoaPesquisaComponent } from './pessoas/pessoa-pesquisa/pessoa-pesquisa.component';
+
+export const routes: Routes = [
+
+    {path: 'lancamentos', component: LancamentosPesquisaComponent
+    },
+    {path: 'pessoas', component: PessoaPesquisaComponent
+    },
+
+    {
+        path: 'pessoa/nova', component: PessoaCadastroComponent
+    },
+    {
+        path: 'lancamento/novo', component: LancamentosCadastroComponent
+    },
+    {
+        path: '**',  redirectTo: 'lancamentos', pathMatch: 'full'
+        },
+
+];
